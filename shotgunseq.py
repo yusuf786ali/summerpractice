0d274e3f38cef402b690bed6b0b547912d9aa135
yusuf786ali <yusuf876ali@gmail.com>
2025-08-29 10:53:47 -0700

    complete

--- shotgunseq.py
+++ shotgunseq.py
@@ -1,28 +1,41 @@
 import sys
 import random
 
-genomesize = sys.argv[1]
-reads = sys.argv[2]
-times = sys.argv[3]
+genomesize = int(sys.argv[1])
+readsize = int(sys.argv[2])
+coverage = int(sys.argv[3])
+limit = int(sys.argv[4])
 
 genome = []
 
 for i in range(int(genomesize)):
     genome.append(0)
 
-for i in range(0, int(times)):
-    value  = random.randint(0, (len(genome)-(int(reads))))
-    value2 = value + int(reads)
-    for j in range(0, len(genome)):
-        if j in range(value, value2):
-        #if (j >= value) and (j <= value2):
-            genome[j] += 1
-    print(value, value2)
+for _ in range(genomesize * coverage//readsize):
+    start  = random.randint(0, (len(genome)-(int(readsize))))
+    stop = start + int(readsize)
+    for j in range(start, stop):
+        genome[j] += 1
 
 total = 0
 for num in genome:
     if num >= 1:
         total += 1
 
-print('% coverage is', ((total/len(genome))*100))
+
+observed = [0] * limit
+
+
+
+for x in genome[readsize:-readsize]:
+    observed[x] += 1
+
+for x in range(limit):
+    print('The value', x, 'is seen', observed[x],
+           'times which is', (observed[x]/(genomesize-readsize*2)*100), 
+           'of total genome')
+
+print(observed)
 print(genome)
+
+#print('% coverage is', ((total/len(genome))*100))
